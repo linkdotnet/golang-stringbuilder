a0f9e965d400c11a9009af56ab9f1a1516f7f1e8
Steven Giesel <stgiesel35@gmail.com>
2022-12-28 09:46:26 +0100

    Added benchmark

---
+++ stringbuilder_benchmark_test.go
@@ -0,0 +1,63 @@
+package Text
+
+import (
+	"strings"
+	"testing"
+)
+
+const text = "Hello dear World"
+const count = 25
+
+var result string
+
+func BenchmarkStringBuilderConcat(b *testing.B) {
+	var r string
+	for n := 0; n < b.N; n++ {
+		r = benchmarkStringBuilderConcat(text, count)
+	}
+	result = r
+}
+
+func BenchmarkStringConcat(b *testing.B) {
+	var r string
+	for n := 0; n < b.N; n++ {
+		r = benchmarkStringConcat(text, count)
+	}
+	result = r
+}
+
+func BenchmarkGoStringBuilderConcat(b *testing.B) {
+	var r string
+	for n := 0; n < b.N; n++ {
+		r = benchmarkGoStringBuilderConcat(text, count)
+	}
+	result = r
+}
+
+func benchmarkStringBuilderConcat(text string, count int) string {
+	s := &StringBuilder{}
+	for i := 0; i < count; i++ {
+		s.Append(text)
+	}
+
+	return s.ToString()
+}
+
+func benchmarkStringConcat(text string, count int) string {
+	s := ""
+	for i := 0; i < count; i++ {
+		s += text
+	}
+
+	return s
+}
+
+func benchmarkGoStringBuilderConcat(text string, count int) string {
+	s := &strings.Builder{}
+
+	for i := 0; i < count; i++ {
+		s.WriteString(text)
+	}
+
+	return s.String()
+}
